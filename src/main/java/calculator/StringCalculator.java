dc457314ff1e4ac864ce73ea6bd49fe52f9e3265
Vaibhav Pimple <pimplevaibhav01@gmail.com>
2021-10-17 15:21:11 +0530

    refactor add method for single input

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,9 +6,20 @@ class StringCalculator {
 
         if(input.equals(""))
             return 0;
+        int sum=0;
 
 
-        return 0;
+        String[] inputs=input.split(",");
+        // System.out.println(Arrays.toString(inputs));
+        if(inputs.length==1){
+            return Integer.valueOf(input);
+        }
+        for(String str : inputs){
+            sum=sum + Integer.valueOf(str);
+        }
+
+
+        return sum;
     }
 
 }
