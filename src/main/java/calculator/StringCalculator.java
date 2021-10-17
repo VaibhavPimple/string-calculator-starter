8e7e2bf0ddc50abf81b0382b3ad32702780fbf02
Vaibhav Pimple <pimplevaibhav01@gmail.com>
2021-10-17 15:16:29 +0530

    checking test for empty string

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,13 @@
 package calculator;
-
+import java.util.*;
 class StringCalculator {
 
     public int add(String input) {
+
+        if(input.equals(""))
+            return 0;
+
+
         return 0;
     }
 
