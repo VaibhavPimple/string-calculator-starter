8060c0cdd8cc6fa3546890b8b6751cc20fefd6eb
Vaibhav Pimple <pimplevaibhav01@gmail.com>
2021-10-17 16:40:30 +0530

    refactor code to allow diffrent dilemeter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,15 +3,25 @@ import java.util.*;
 class StringCalculator {
 
     public int add(String input) throws NegetivesNotAllowedException {
-        input=input.replace("\n",",");
+        char delimeter=',';
+        String sDelimeter=null;
+        String newInput=null;
+        if(input.startsWith("//")){
+            delimeter=input.charAt(2);
+            newInput=input.substring(3);
+        }
+        System.out.println(input);
+        System.out.println(newInput);
+        sDelimeter=Character.toString(delimeter);
+        input=input.replace("\n",sDelimeter);
 
 
-        if(input.equals(""))
+        if(newInput.equals(""))
             return 0;
         int sum=0;
 
 
-        String[] inputs=input.split(",");
+        String[] inputs=newInput.split(sDelimeter);
         // System.out.println(Arrays.toString(inputs));
         List<Integer> netiveNumberList=new LinkedList<>();
         for(String str :inputs){
@@ -19,9 +29,9 @@ class StringCalculator {
                 netiveNumberList.add(Integer.valueOf(str));
         }
         if(inputs.length==1){
-            if(Integer.valueOf(input)>1000)
+            if(Integer.valueOf(newInput)>1000)
                 return 0;
-            if(Integer.valueOf(input)<0)
+            if(Integer.valueOf(newInput)<0)
                 throw new NegetivesNotAllowedException("Negetive value not allowed -"+netiveNumberList.toString());
             return Integer.valueOf(input);
         }
